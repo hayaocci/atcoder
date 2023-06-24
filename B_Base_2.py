886adafae76a132228e83d8517128df06f4dd076
Shun Taguchi <107763324+hayaocci@users.noreply.github.com>
2023-06-25 00:47:05 +0900

    B_Base_2の完成

--- B_Base_2.py
+++ B_Base_2.py
@@ -4,19 +4,24 @@
 #入力
 a = input()
 
-
-
 #処理
 length = len(a)
+
+#スペースの消去
 i = 0
+input_number =""
+while i < length:
+    input_number = input_number + a[i]
+    i = i + 2
+
+#結果の計算
 j = 0
 result = 0
-while i < length:
+input_number_length = len(input_number)
 
-    while j < 64:
-        result = result + str(a[i])**j
-
-    i = i + 2
+while j < input_number_length:
+    result = result + int(input_number[j])*(2**j)
+    j = j + 1
 
 #出力
 print(result)
