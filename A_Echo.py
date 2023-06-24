5dd8caa7dad8df77e4870e5d1eeaa58e3055f2a3
Shun Taguchi <107763324+hayaocci@users.noreply.github.com>
2023-06-24 23:12:34 +0900

    お試し

--- A_Echo.py
+++ A_Echo.py
@@ -1,11 +1,6 @@
 #20230624
 #A - Echo
 
-def list_split(list, n):
-    for i in range(0, len(list), n):
-        yield list[i:i + n]
-        
-
 #文字列の入力
 print("文字列を入力してください")
 n = input()
@@ -17,17 +12,9 @@ print("入力した文字列は" + n)
 s = len(n)
 print("文字列の長さは" + str(s))
 
-o = list(n)
-print(o)
-
-o1 = list(list_split(o, 1))
-print(o1)
-
-'''
 i = 0
 while i < s:
-    output= n[i] + n[i]
-    print(output1)
-
+    output = n[i] + n[i]
     i = i + 1
-'''
+
+print(output)
