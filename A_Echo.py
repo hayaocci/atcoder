ccdd93c6eb77aad4e3430a2489e9da55f7955601
Shun Taguchi <107763324+hayaocci@users.noreply.github.com>
2023-06-24 22:44:47 +0900

    お試し

--- A_Echo.py
+++ A_Echo.py
@@ -2,6 +2,7 @@
 #A - Echo
 
 #文字列の入力
+print("文字列を入力してください")
 n = input()
 
 #入力した文字列の確認
@@ -13,7 +14,7 @@ print("文字列の長さは" + str(s))
 
 i = 0
 while i < s:
-    output1 = n[i]
+    output1 = n[i] + n[i]
     print(output1)
 
     i = i + 1
