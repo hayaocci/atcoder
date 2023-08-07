e93d4cacca8d03cf21f2cacdc5ab92bafc0437bb
Shun Taguchi <107763324+hayaocci@users.noreply.github.com>
2023-08-07 14:17:20 +0900

    a

---
+++ python/A_Chord.py
@@ -0,0 +1,15 @@
+'''
+https://atcoder.jp/contests/abc312/tasks/abc312_a
+
+一発AC
+
+'''
+
+S_list= ['ACE', 'BDF','CEG', 'DFA', 'EGB', 'FAC', 'GBD']
+
+S = input()
+
+if S in S_list:
+    print('Yes')
+else:
+    print('No')
